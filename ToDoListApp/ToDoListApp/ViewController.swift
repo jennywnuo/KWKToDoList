a553a702ddac7f7b6869a3cb41ba1558a75e75a9
jennywnuo <52713614+jennywnuo@users.noreply.github.com>
2020-07-28 23:57:41 -0700

    followed video instructions

---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by jenny nuo on 7/28/20.
+//  Copyright © 2020 jenny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
